e5d92c78158cd5e42d181e5e8cd2bcea9fd89e58
sowrd <15159885771@163.com>
2022-08-05 11:25:48 +0800

    消息队列设置

---
+++ pay-components/pay-components-mq/src/main/java/com/pay/components/mq/constant/MQVenderCS.java
@@ -0,0 +1,17 @@
+package com.pay.components.mq.constant;
+
+/**
+ * Description： MQ厂商定义类
+ *
+ * @author: 段世超
+ * @aate: Created in 2022/8/5 11:00
+ */
+public class MQVenderCS {
+
+    public static final String YML_VENDER_KEY = "isys.mq.vender";
+
+    public static final String ACTIVE_MQ = "activeMQ";
+    public static final String RABBIT_MQ = "rabbitMQ";
+    public static final String ROCKET_MQ = "rocketMQ";
+    public static final String ALIYUN_ROCKET_MQ = "aliYunRocketMQ";
+}
---
+++ pay-components/pay-components-mq/src/main/java/com/pay/components/mq/model/CleanMchLoginAuthCacheMQ.java
@@ -0,0 +1,18 @@
+package com.pay.components.mq.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Description： 定义MQ消息格式
+ * 业务场景：【清楚商户登录信息】
+ *
+ * @author: 段世超
+ * @aate: Created in 2022/8/5 11:20
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CleanMchLoginAuthCacheMQ {
+}
