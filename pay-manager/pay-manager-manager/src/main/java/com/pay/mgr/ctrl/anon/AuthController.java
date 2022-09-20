659073b002f3f95ad0116e3b41a00679b377087f
sowrd <15159885771@163.com>
2022-09-20 18:41:24 +0800

    系统配置信息

--- pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/anon/AuthController.java
+++ pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/anon/AuthController.java
@@ -1,11 +1,6 @@
 package com.pay.mgr.ctrl.anon;
 
-import com.pay.mgr.config.SysTemYmlConfig;
 import com.pay.mgr.ctrl.common.CommonCtrl;
-import com.pay.pay.core.model.security.JeeUserDetails;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import javax.annotation.Resource;
 
 /**
  * Description： 认证接口
@@ -14,14 +9,4 @@ import javax.annotation.Resource;
  * @aate: Created in 2022/9/19 18:35
  */
 public abstract class AuthController extends CommonCtrl {
-
-    @Resource
-    protected SysTemYmlConfig mainConfig;
-
-
-    /** 获取当前用户ID */
-    protected JeeUserDetails getCurrentUser(){
-
-        return (JeeUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
 }
--- pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/common/CommonCtrl.java
+++ pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/common/CommonCtrl.java
@@ -1,10 +1,34 @@
 package com.pay.mgr.ctrl.common;
 
+import com.pay.mgr.config.SysTemYmlConfig;
+import com.pay.pay.core.ctrls.AbstractCtrl;
+import com.pay.pay.core.model.security.JeeUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 /**
  * Description： TODO
  *
  * @author: 段世超
  * @aate: Created in 2022/9/19 18:37
  */
-public class CommonCtrl {
+public class CommonCtrl extends AbstractCtrl {
+
+    @Autowired
+    protected SysTemYmlConfig mainConfig;
+
+    /** 获取当前用户ID */
+    protected JeeUserDetails getCurrentUser(){
+
+        return (JeeUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    /**
+     * 获取当前用户登录IP
+     * @return
+     */
+    protected String getIp() {
+        return getClientIp();
+    }
+
 }
