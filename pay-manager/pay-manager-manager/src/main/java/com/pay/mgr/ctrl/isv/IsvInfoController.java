886c727d942c41262cd24f96905f78f08d229dcd
sowrd <15159885771@163.com>
2022-09-23 18:41:44 +0800

    系统配置信息

--- pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/isv/IsvInfoController.java
+++ pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/isv/IsvInfoController.java
@@ -1,15 +1,20 @@
 package com.pay.mgr.ctrl.isv;
 
+import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.pay.components.mq.vender.IMQSender;
 import com.pay.mgr.ctrl.common.CommonCtrl;
+import com.pay.pay.core.aop.MethodLog;
+import com.pay.pay.core.constants.ApiCodeEnum;
 import com.pay.pay.core.entity.IsvInfo;
 import com.pay.pay.core.model.ApiRes;
 import com.pay.pay.service.impl.IsvInfoService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -52,5 +57,25 @@ public class IsvInfoController extends CommonCtrl {
        return ApiRes.page(pages);
    }
 
+    /**
+     * 新增服务商信息
+     * @return
+     */
+    @PreAuthorize("hasAuthority('ENT_ISV_INFO_ADD')")
+    @MethodLog(remark = "新增服务商")
+    @RequestMapping(value="", method = RequestMethod.POST)
+    public ApiRes add() {
 
-}
+        IsvInfo isvInfo = getObject(IsvInfo.class);
+        String isvNo = "V" + DateUtil.currentSeconds();
+        isvInfo.setIsvNo(isvNo);
+        isvInfo.setCreatedUid(getCurrentUser().getSysUser().getSysUserId());
+        isvInfo.setCreatedBy(getCurrentUser().getSysUser().getRealname());
+        boolean result = isvInfoService.save(isvInfo);
+        if (!result) {
+            return ApiRes.fail(ApiCodeEnum.SYS_OPERATION_FAIL_CREATE);
+        }
+        return ApiRes.ok();
+    }
+
+   }
