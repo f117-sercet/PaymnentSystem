b60daf4315b99f21991b9ba913cd580a8b3fdba9
sowrd <15159885771@163.com>
2022-09-24 20:39:25 +0800

    系统配置信息

--- pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/isv/IsvInfoController.java
+++ pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/isv/IsvInfoController.java
@@ -3,6 +3,7 @@ package com.pay.mgr.ctrl.isv;
 import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.pay.components.mq.model.ResetIsvMchAppInfoConfigMQ;
 import com.pay.components.mq.vender.IMQSender;
 import com.pay.mgr.ctrl.common.CommonCtrl;
 import com.pay.pay.core.aop.MethodLog;
@@ -13,6 +14,7 @@ import com.pay.pay.service.impl.IsvInfoService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -77,5 +79,14 @@ public class IsvInfoController extends CommonCtrl {
         }
         return ApiRes.ok();
     }
+    @PreAuthorize("hasAuthority('ENT_ISV_INFO_DEL')")
+    @MethodLog(remark = "删除服务商")
+    @RequestMapping(value="/{isvNo}", method = RequestMethod.DELETE)
+    public ApiRes delete(@PathVariable("isvNo") String isvNo) {
+        isvInfoService.removeByIsvNo(isvNo);
 
+        // 推送mq到目前节点进行更新数据
+        mqSender.send(ResetIsvMchAppInfoConfigMQ.build(ResetIsvMchAppInfoConfigMQ.RESET_TYPE_ISV_INFO, isvNo, null, null));
+        return ApiRes.ok();
+    }
    }
