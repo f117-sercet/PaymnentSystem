3314b2f69315427712d7ae8e0c7c3a17b66fb392
sowrd <15159885771@163.com>
2022-10-20 18:34:41 +0800

    toke能配置

--- pay-core/src/main/java/com/pay/pay/core/cache/ITokenService.java
+++ pay-core/src/main/java/com/pay/pay/core/cache/ITokenService.java
@@ -1,5 +1,7 @@
 package com.pay.pay.core.cache;
 
+import com.pay.pay.core.model.security.JeeUserDetails;
+
 /**
  * Description： token service
  *
@@ -7,4 +9,16 @@ package com.pay.pay.core.cache;
  * @aate: Created in 2022/10/14 18:12
  */
 public class ITokenService {
+
+    /***
+     * 处理Token信息
+     * @param userDetail
+     * @param cacheKey
+     */
+    public static void processTokenCache(JeeUserDetails userDetail, String cacheKey){
+
+        //设置cacheKey
+        userDetail.setCacheKey(cacheKey);
+    }
+
 }
