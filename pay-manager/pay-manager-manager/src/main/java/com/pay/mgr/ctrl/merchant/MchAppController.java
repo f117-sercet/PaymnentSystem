25aa212201bdd26fa1616e6c63cab583736e143b
sowrd <15159885771@163.com>
2022-10-31 18:24:41 +0800

    商户应用配置

---
+++ pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/merchant/MchAppController.java
@@ -0,0 +1,121 @@
+package com.pay.mgr.ctrl.merchant;
+
+import cn.hutool.core.util.IdUtil;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.pay.components.mq.model.ResetIsvMchAppInfoConfigMQ;
+import com.pay.components.mq.vender.IMQSender;
+import com.pay.mgr.ctrl.common.CommonCtrl;
+import com.pay.pay.core.aop.MethodLog;
+import com.pay.pay.core.constants.ApiCodeEnum;
+import com.pay.pay.core.entity.MchApp;
+import com.pay.pay.core.model.ApiRes;
+import com.pay.pay.service.impl.MchAppService;
+import com.pay.pay.service.impl.MchInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Description： 商户应用管理类
+ *
+ * @author: 段世超
+ * @aate: Created in 2022/10/27 19:15
+ */
+public class MchAppController extends CommonCtrl {
+
+    @Autowired private MchInfoService mchInfoService;
+    @Autowired private MchAppService mchAppService;
+    @Autowired private IMQSender mqSender;
+
+    /**
+     * @Author: ZhuXiao
+     * @Description: 应用列表
+     * @Date: 9:59 2021/6/16
+     */
+    @PreAuthorize("hasAuthority('ENT_MCH_APP_LIST')")
+    @GetMapping
+    public ApiRes list() {
+        MchApp mchApp = getObject(MchApp.class);
+
+        IPage<MchApp> pages = mchAppService.selectPage(getIPage(), mchApp);
+        return ApiRes.ok(pages);
+    }
+
+    /**
+     * @Author: ZhuXiao
+     * @Description: 新建应用
+     * @Date: 10:05 2021/6/16
+     */
+    @PreAuthorize("hasAuthority('ENT_MCH_APP_ADD')")
+    @MethodLog(remark = "新建应用")
+    @PostMapping
+    public ApiRes add() {
+        MchApp mchApp = getObject(MchApp.class);
+        mchApp.setAppId(IdUtil.objectId());
+
+        if(mchInfoService.getById(mchApp.getMchNo()) == null) {
+            return ApiRes.fail(ApiCodeEnum.SYS_OPERATION_FAIL_SELETE);
+        }
+
+        boolean result = mchAppService.save(mchApp);
+        if (!result) {
+            return ApiRes.fail(ApiCodeEnum.SYS_OPERATION_FAIL_CREATE);
+        }
+        return ApiRes.ok();
+    }
+
+    /**
+     * @Author: ZhuXiao
+     * @Description: 应用详情
+     * @Date: 10:13 2021/6/16
+     */
+    @PreAuthorize("hasAnyAuthority('ENT_MCH_APP_VIEW', 'ENT_MCH_APP_EDIT')")
+    @GetMapping("/{appId}")
+    public ApiRes detail(@PathVariable("appId") String appId) {
+        MchApp mchApp = mchAppService.selectById(appId);
+        if (mchApp == null) {
+            return ApiRes.fail(ApiCodeEnum.SYS_OPERATION_FAIL_SELETE);
+        }
+
+        return ApiRes.ok(mchApp);
+    }
+
+    /**
+     * @Author: ZhuXiao
+     * @Description: 更新应用信息
+     * @Date: 10:11 2021/6/16
+     */
+    @PreAuthorize("hasAuthority('ENT_MCH_APP_EDIT')")
+    @MethodLog(remark = "更新应用信息")
+    @PutMapping("/{appId}")
+    public ApiRes update(@PathVariable("appId") String appId) {
+        MchApp mchApp = getObject(MchApp.class);
+        mchApp.setAppId(appId);
+        boolean result = mchAppService.updateById(mchApp);
+        if (!result) {
+            return ApiRes.fail(ApiCodeEnum.SYS_OPERATION_FAIL_UPDATE);
+        }
+        // 推送修改应用消息
+        mqSender.send(ResetIsvMchAppInfoConfigMQ.build(ResetIsvMchAppInfoConfigMQ.RESET_TYPE_MCH_APP, null, mchApp.getMchNo(), appId));
+        return ApiRes.ok();
+    }
+
+    /**
+     * @Author: ZhuXiao
+     * @Description: 删除应用
+     * @Date: 10:14 2021/6/16
+     */
+    @PreAuthorize("hasAuthority('ENT_MCH_APP_DEL')")
+    @MethodLog(remark = "删除应用")
+    @DeleteMapping("/{appId}")
+    public ApiRes delete(@PathVariable("appId") String appId) {
+
+        MchApp mchApp = mchAppService.getById(appId);
+        mchAppService.removeByAppId(appId);
+
+        // 推送mq到目前节点进行更新数据
+        mqSender.send(ResetIsvMchAppInfoConfigMQ.build(ResetIsvMchAppInfoConfigMQ.RESET_TYPE_MCH_APP, null, mchApp.getMchNo(), appId));
+        return ApiRes.ok();
+    }
+
+}
---
+++ pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/merchant/MchInfoController.java
@@ -0,0 +1,140 @@
+package com.pay.mgr.ctrl.merchant;
+
+import cn.hutool.core.date.DateUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.pay.components.mq.model.CleanMchLoginAuthCacheMQ;
+import com.pay.components.mq.model.ResetIsvMchAppInfoConfigMQ;
+import com.pay.components.mq.vender.IMQSender;
+import com.pay.mgr.ctrl.common.CommonCtrl;
+import com.pay.pay.core.aop.MethodLog;
+import com.pay.pay.core.constants.CS;
+import com.pay.pay.core.entity.MchInfo;
+import com.pay.pay.core.entity.SysUser;
+import com.pay.pay.core.model.ApiRes;
+import com.pay.pay.service.impl.MchInfoService;
+import com.pay.pay.service.impl.SysUserAuthService;
+import com.pay.pay.service.impl.SysUserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Description： 商户管理类
+ *
+ * @author: 段世超
+ * @aate: Created in 2022/10/31 17:38
+ */
+@RestController
+@RequestMapping("/api/mchInfo")
+public class MchInfoController extends CommonCtrl {
+
+    @Autowired
+    private MchInfoService mchInfoService;
+    @Autowired
+    private SysUserService sysUserService;
+    @Autowired
+    private SysUserAuthService sysUserAuthService;
+    @Autowired
+    private IMQSender mqSender;
+
+
+    @PreAuthorize("hasAuthority('ENT_MCH_LIST')")
+    @RequestMapping(value="", method = RequestMethod.GET)
+    public ApiRes list(){
+
+        MchInfo mchInfo= getObject(MchInfo.class);
+
+        LambdaQueryWrapper<MchInfo> wrapper = MchInfo.gw();
+        if (StringUtils.isNotEmpty(mchInfo.getMchNo())) {
+            wrapper.eq(MchInfo::getMchNo, mchInfo.getMchNo());
+        }
+        if (StringUtils.isNotEmpty(mchInfo.getIsvNo())) {
+            wrapper.eq(MchInfo::getIsvNo, mchInfo.getIsvNo());
+        }
+        if (StringUtils.isNotEmpty(mchInfo.getMchName())) {
+            wrapper.eq(MchInfo::getMchName, mchInfo.getMchName());
+        }
+        if (mchInfo.getType() != null) {
+            wrapper.eq(MchInfo::getType, mchInfo.getType());
+        }
+        if (mchInfo.getState() != null) {
+            wrapper.eq(MchInfo::getState, mchInfo.getState());
+        }
+        wrapper.orderByDesc(MchInfo::getCreatedAt);
+
+        IPage<MchInfo> pages = mchInfoService.page(getIPage(), wrapper);
+        return ApiRes.page(pages);
+
+    }
+
+    /**
+     * @author: pangxiaoyu
+     * @date: 2021/6/7 16:14
+     * @describe: 新增商户信息
+     */
+    @PreAuthorize("hasAuthority('ENT_MCH_INFO_ADD')")
+    @MethodLog(remark = "新增商户")
+    @RequestMapping(value="", method = RequestMethod.POST)
+    public ApiRes add(){
+        MchInfo mchInfo = getObject(MchInfo.class);
+
+        // 获取传入的商户登录名
+        String loginUserName = getValStringRequired("loginUserName");
+        mchInfo.setMchNo("M" + DateUtil.currentSeconds());
+        // 当前登录用户信息
+        SysUser sysUser = getCurrentUser().getSysUser();
+        mchInfo.setCreatedUid(sysUser.getSysUserId());
+        mchInfo.setCreatedBy(sysUser.getRealname());
+
+        mchInfoService.addMch(mchInfo, loginUserName);
+        return ApiRes.ok();
+    }
+    /**
+     * @author: 段世超
+     * @date: 2021/6/7 16:14
+     * @describe: 删除商户信息
+     */
+    @PreAuthorize("hasAuthority('ENT_MCH_INFO_DEL')")
+    @MethodLog(remark = "删除商户")
+    @RequestMapping(value="/{mchNo}", method = RequestMethod.DELETE)
+    public ApiRes delete(@PathVariable("mchNo") String mchNo) {
+        List<Long> userIdList = mchInfoService.removeByMchNo(mchNo);
+
+        // 推送mq删除redis用户缓存
+        mqSender.send(CleanMchLoginAuthCacheMQ.build(userIdList));
+
+        // 推送mq到目前节点进行更新数据
+        mqSender.send(ResetIsvMchAppInfoConfigMQ.build(ResetIsvMchAppInfoConfigMQ.RESET_TYPE_MCH_INFO, null, mchNo, null));
+        return ApiRes.ok();
+    }
+
+    @PreAuthorize("hasAuthority('ENT_MCH_INFO_EDIT')")
+    @MethodLog(remark = "更新商户信息")
+    @RequestMapping(value="/{mchNo}", method = RequestMethod.PUT)
+    public ApiRes update(@PathVariable("mchNo") String mchNo){
+        MchInfo mchInfo = getObject(MchInfo.class);
+        mchInfo.setMchNo(mchNo); // 设置商户号主键
+
+        mchInfo.setType(null); // 防止变更商户类型
+        mchInfo.setIsvNo(null);
+
+        // 待删除用户登录信息的ID list
+        Set<Long> removeCacheUserIdList = new HashSet<>();
+
+        // 如果 商户状态为禁用状态,清除该商户的登录信息
+        if(mchInfo.getState() == CS.NO){
+
+            sysUserAuthService.list();
+        }
+
+    }
+}
