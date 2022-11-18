d979182723c56a6f66fb7f1ea2f123f1ebb370b6
sowrd <15159885771@163.com>
2022-11-18 19:19:06 +0800

    商户应用配置

--- pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/merchant/MchPayPassageConfigController.java
+++ pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/merchant/MchPayPassageConfigController.java
@@ -3,7 +3,9 @@ package com.pay.mgr.ctrl.merchant;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.pay.mgr.ctrl.common.CommonCtrl;
+import com.pay.pay.core.entity.MchPayPassage;
 import com.pay.pay.core.entity.PayWay;
 import com.pay.pay.core.model.ApiRes;
 import com.pay.pay.service.impl.MchAppService;
@@ -13,6 +15,9 @@ import com.pay.pay.service.impl.PayWayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Description： 商户支付通道管理类
@@ -53,6 +58,20 @@ public class MchPayPassageConfigController extends CommonCtrl {
             wrapper.like(PayWay::getWayName,wayName);
         }
         IPage<PayWay> payWayPage = payWayService.page(getIPage(),wrapper);
+        if (!CollectionUtils.isEmpty(payWayPage.getRecords())){
+
+            // 支付方式代码集合
+            List<String> wayCodeList = new LinkedList<>();
+            payWayPage.getRecords().stream().forEach(payWay->wayCodeList.add(payWay.getWayCode()));
+
+            // 应用支付通道集合
+            // 应用支付通道集合
+            List<MchPayPassage> mchPayPassageList = mchPayPassageService.list(MchPayPassage.gw()
+                    .select(MchPayPassage::getWayCode, MchPayPassage::getState)
+                    .eq(MchPayPassage::getAppId, appId)
+                    .in(MchPayPassage::getWayCode, wayCodeList));
+
+        }
         return ApiRes.ok();
     }
 
