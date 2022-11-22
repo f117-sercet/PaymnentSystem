ce375b871426ce440fb4812dd1c3a216151a0af4
sowrd <15159885771@163.com>
2022-11-22 21:26:38 +0800

    商户应用配置

--- pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/merchant/MchPayPassageConfigController.java
+++ pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/merchant/MchPayPassageConfigController.java
@@ -1,11 +1,14 @@
 package com.pay.mgr.ctrl.merchant;
 
 import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.pay.mgr.ctrl.common.CommonCtrl;
+import com.pay.pay.core.constants.ApiCodeEnum;
 import com.pay.pay.core.constants.CS;
+import com.pay.pay.core.entity.MchApp;
+import com.pay.pay.core.entity.MchInfo;
 import com.pay.pay.core.entity.MchPayPassage;
 import com.pay.pay.core.entity.PayWay;
 import com.pay.pay.core.model.ApiRes;
@@ -14,6 +17,10 @@ import com.pay.pay.service.impl.MchInfoService;
 import com.pay.pay.service.impl.MchPayPassageService;
 import com.pay.pay.service.impl.PayWayService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,26 +51,26 @@ public class MchPayPassageConfigController extends CommonCtrl {
      * 查询支付方式列表,并添加是否支持配置支付通道状态
      * @return
      */
-    public ApiRes list(){
+    public ApiRes list() {
         String appId = getValStringRequired("appId");
         String wayCode = getValString("wayCode");
         String wayName = getValString("wayName");
 
-        // 支付方式集合
+        //支付方式集合
         LambdaQueryWrapper<PayWay> wrapper = PayWay.gw();
-
         if (StrUtil.isNotBlank(wayCode)) {
-            wrapper.eq(PayWay::getWayCode,wayCode);
+            wrapper.eq(PayWay::getWayCode, wayCode);
         }
         if (StrUtil.isNotBlank(wayName)) {
-            wrapper.like(PayWay::getWayName,wayName);
+            wrapper.like(PayWay::getWayName, wayName);
         }
-        IPage<PayWay> payWayPage = payWayService.page(getIPage(),wrapper);
-        if (!CollectionUtils.isEmpty(payWayPage.getRecords())){
+        IPage<PayWay> payWayPage = payWayService.page(getIPage(), wrapper);
+
+        if (!CollectionUtils.isEmpty(payWayPage.getRecords())) {
 
             // 支付方式代码集合
             List<String> wayCodeList = new LinkedList<>();
-            payWayPage.getRecords().stream().forEach(payWay->wayCodeList.add(payWay.getWayCode()));
+            payWayPage.getRecords().stream().forEach(payWay -> wayCodeList.add(payWay.getWayCode()));
 
             // 应用支付通道集合
             List<MchPayPassage> mchPayPassageList = mchPayPassageService.list(MchPayPassage.gw()
@@ -71,14 +78,45 @@ public class MchPayPassageConfigController extends CommonCtrl {
                     .eq(MchPayPassage::getAppId, appId)
                     .in(MchPayPassage::getWayCode, wayCodeList));
 
-            for (PayWay payWay : payWayPage.getRecords()){
+            for (PayWay payWay : payWayPage.getRecords()) {
                 payWay.addExt("passageState", CS.NO);
-                for (MchPayPassage mchPayPassage : mchPayPassageList){
-
+                for (MchPayPassage mchPayPassage : mchPayPassageList) {
+                    // 某种支付方式多个通道的情况下，只要有一个通道状态为开启，则该支付方式对应为开启状态
+                    if (payWay.getWayCode().equals(mchPayPassage.getWayCode()) && mchPayPassage.getState() == CS.YES) {
+                        payWay.addExt("passageState", CS.YES);
+                        break;
+                    }
                 }
             }
         }
-        return ApiRes.ok();
+
+        return ApiRes.page(payWayPage);
     }
 
+    /**
+     * 根据appId,支付方式查询可用的支付接口列表
+     *
+     * @param appId
+     * @param wayCode
+     * @return
+     */
+    @PreAuthorize("hasAuthority('ENT_MCH_PAY_PASSAGE_CONFIG')")
+    @GetMapping("/availablePayInterface/{appId}/{wayCode}")
+    public ApiRes availablePayInterface(@PathVariable("appId") String appId, @PathVariable("wayCode") String wayCode) {
+
+        MchApp mchApp = mchAppService.getById(appId);
+        if (mchApp == null || mchApp.getState() != CS.YES) {
+            return ApiRes.fail(ApiCodeEnum.SYS_OPERATION_FAIL_SELETE);
+        }
+
+        MchInfo mchInfo = mchInfoService.getById(mchApp.getMchNo());
+        if (mchInfo == null || mchInfo.getState() != CS.YES) {
+            return ApiRes.fail(ApiCodeEnum.SYS_OPERATION_FAIL_SELETE);
+        }
+
+        // 根据支付方式查询可用支付接口列表
+        List<JSONObject> list = mchPayPassageService.selectAvailablePayInterfaceList(wayCode, appId, CS.INFO_TYPE_MCH_APP, mchInfo.getType());
+
+        return ApiRes.ok(list);
+    }
 }
