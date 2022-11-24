25b0f09b2b8cce7f696e042a9d86815f75caa92c
sowrd <15159885771@163.com>
2022-11-24 16:30:20 +0800

    商户应用配置

--- pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/merchant/MchPayPassageConfigController.java
+++ pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/merchant/MchPayPassageConfigController.java
@@ -1,16 +1,19 @@
 package com.pay.mgr.ctrl.merchant;
 
 import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.pay.mgr.ctrl.common.CommonCtrl;
+import com.pay.pay.core.aop.MethodLog;
 import com.pay.pay.core.constants.ApiCodeEnum;
 import com.pay.pay.core.constants.CS;
 import com.pay.pay.core.entity.MchApp;
 import com.pay.pay.core.entity.MchInfo;
 import com.pay.pay.core.entity.MchPayPassage;
 import com.pay.pay.core.entity.PayWay;
+import com.pay.pay.core.exeception.BizException;
 import com.pay.pay.core.model.ApiRes;
 import com.pay.pay.service.impl.MchAppService;
 import com.pay.pay.service.impl.MchInfoService;
@@ -19,10 +22,7 @@ import com.pay.pay.service.impl.PayWayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -119,4 +119,26 @@ public class MchPayPassageConfigController extends CommonCtrl {
 
         return ApiRes.ok(list);
     }
-}
+    @PreAuthorize("hasAuthority('ENT_MCH_PAY_PASSAGE_ADD')")
+    @PostMapping
+    @MethodLog(remark = "更新商户支付通道")
+    public ApiRes saveOrUpdate(){
+
+        String reqParams = getValStringRequired("reqParam");
+        try {
+            List<MchPayPassage> mchPayPassageList = JSONArray.parseArray(reqParams, MchPayPassage.class);
+            if (CollectionUtils.isEmpty(mchPayPassageList)) {
+                throw new BizException("操作失败");
+            }
+            MchApp mchApp = mchAppService.getById(mchPayPassageList.get(0).getAppId());
+            if (mchApp == null || mchApp.getState() != CS.YES) {
+                return ApiRes.fail(ApiCodeEnum.SYS_OPERATION_FAIL_SELETE);
+            }
+
+            mchPayPassageService.saveOrUpdateBatchSelf(mchPayPassageList, mchApp.getMchNo());
+            return ApiRes.ok();
+        }catch (Exception e) {
+            return ApiRes.fail(ApiCodeEnum.SYSTEM_ERROR);
+        }
+    }
+    }
