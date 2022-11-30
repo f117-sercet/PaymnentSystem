b9cdc02e5a548bcfd6157b6a36be17b1c22172b7
sowrd <15159885771@163.com>
2022-11-30 16:13:48 +0800

    系统应用配置

--- pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/order/TransferOrderController.java
+++ pay-manager/pay-manager-manager/src/main/java/com/pay/mgr/ctrl/order/TransferOrderController.java
@@ -5,9 +5,9 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.pay.mgr.ctrl.common.CommonCtrl;
 import com.pay.pay.core.constants.ApiCodeEnum;
-import com.pay.pay.core.entity.RefundOrder;
+import com.pay.pay.core.entity.TransferOrder;
 import com.pay.pay.core.model.ApiRes;
-import com.pay.pay.service.impl.RefundOrderService;
+import com.pay.pay.service.impl.TransferOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,43 +16,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Description： 退款订单类型
+ * Description： 转账订单
  *
  * @author: 段世超
- * @aate: Created in 2022/11/29 22:32
+ * @aate: Created in 2022/11/30 16:12
  */
 @RestController
-@RequestMapping("/api/refundOrder")
-public class RefundOrderController extends CommonCtrl {
+@RequestMapping("/api/transferOrders")
+public class TransferOrderController extends CommonCtrl {
 
     @Autowired
-    private RefundOrderService refundOrderService;
-
-    /**
-     * 退款订单列表
-     * @return
-     */
-    @PreAuthorize("hasAuthority('ENT_REFUND_LIST')")
+    private TransferOrderService transferOrderService;
+    /** list **/
+    @PreAuthorize("hasAuthority('ENT_TRANSFER_ORDER_LIST')")
     @RequestMapping(value="", method = RequestMethod.GET)
     public ApiRes list() {
 
-        RefundOrder refundOrder = getObject(RefundOrder.class);
+        TransferOrder transferOrder = getObject(TransferOrder.class);
         JSONObject paramJSON = getReqParamJSON();
-        LambdaQueryWrapper<RefundOrder> wrapper = RefundOrder.gw();
-        IPage<RefundOrder> pages = refundOrderService.pageList(getIPage(), wrapper, refundOrder, paramJSON);
+        LambdaQueryWrapper<TransferOrder> wrapper = TransferOrder.gw();
+        IPage<TransferOrder> pages = transferOrderService.pageList(getIPage(), wrapper, transferOrder, paramJSON);
 
         return ApiRes.page(pages);
     }
 
-    /**
-     * 退款订单信息
-     * @param refundOrderId
-     * @return
-     */
-    @PreAuthorize("hasAuthority('ENT_REFUND_ORDER_VIEW')")
-    @RequestMapping(value="/{refundOrderId}", method = RequestMethod.GET)
-    public ApiRes detail(@PathVariable("refundOrderId") String refundOrderId) {
-        RefundOrder refundOrder = refundOrderService.getById(refundOrderId);
+    /** detail **/
+    @PreAuthorize("hasAuthority('ENT_TRANSFER_ORDER_VIEW')")
+    @RequestMapping(value="/{recordId}", method = RequestMethod.GET)
+    public ApiRes detail(@PathVariable("recordId") String transferId) {
+        TransferOrder refundOrder = transferOrderService.getById(transferId);
         if (refundOrder == null) {
             return ApiRes.fail(ApiCodeEnum.SYS_OPERATION_FAIL_SELETE);
         }
