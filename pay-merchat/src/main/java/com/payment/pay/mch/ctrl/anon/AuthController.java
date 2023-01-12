0bceaf3a9844ea6db7b05bc73dacd8ebb170da43
sowrd <15159885771@163.com>
2023-01-12 18:17:17 +0800

    merchat

---
+++ pay-merchat/src/main/java/com/payment/pay/mch/ctrl/anon/AuthController.java
@@ -0,0 +1,16 @@
+package com.payment.pay.mch.ctrl.anon;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description： 登录鉴权
+ *
+ * @author: 段世超
+ * @aate: Created in 2023/1/12 18:13
+ */
+
+@RestController
+@RequestMapping("/api/anon/auth")
+public class AuthController extends CommonCtrl {
+}
---
+++ pay-merchat/src/main/java/com/payment/pay/mch/ctrl/anon/CommonCtrl.java
@@ -0,0 +1,12 @@
+package com.payment.pay.mch.ctrl.anon;
+
+import com.pay.pay.core.ctrls.AbstractCtrl;
+
+/**
+ * Description： TODO
+ *
+ * @author: 段世超
+ * @aate: Created in 2023/1/12 18:14
+ */
+public abstract class CommonCtrl  extends AbstractCtrl {
+}
