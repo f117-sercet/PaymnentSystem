576fba2c468d137bf046625204df3548a6094e64
sowrd <15159885771@163.com>
2023-01-13 14:39:55 +0800

    merchat

--- pay-merchat/src/main/java/com/payment/pay/mch/ctrl/anon/CommonCtrl.java
+++ pay-merchat/src/main/java/com/payment/pay/mch/ctrl/anon/CommonCtrl.java
@@ -3,10 +3,12 @@ package com.payment.pay.mch.ctrl.anon;
 import com.pay.pay.core.ctrls.AbstractCtrl;
 
 /**
- * Description： TODO
+ * 抽象公共Ctrl
  *
  * @author: 段世超
  * @aate: Created in 2023/1/12 18:14
  */
 public abstract class CommonCtrl  extends AbstractCtrl {
+
+
 }
