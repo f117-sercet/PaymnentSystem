168e11ed711b5181ca0912354660ccddeacc02da
sowrd <15159885771@163.com>
2023-01-15 21:11:54 +0800

    权限管理

--- pay-merchat/src/main/java/com/payment/pay/mch/ctrl/anon/CommonCtrl.java
+++ pay-merchat/src/main/java/com/payment/pay/mch/ctrl/anon/CommonCtrl.java
@@ -1,6 +1,10 @@
 package com.payment.pay.mch.ctrl.anon;
 
+import com.pay.pay.core.constants.ApiCodeEnum;
+import com.pay.pay.core.constants.CS;
 import com.pay.pay.core.ctrls.AbstractCtrl;
+import com.pay.pay.core.entity.SysUser;
+import com.pay.pay.core.model.ApiRes;
 import com.pay.pay.core.model.security.JeeUserDetails;
 import com.pay.pay.service.impl.SysConfigService;
 import com.payment.pay.mch.config.SystemYmlConfig;
@@ -22,9 +26,37 @@ public abstract class CommonCtrl  extends AbstractCtrl {
     @Resource
     private SysConfigService sysConfigService;
 
-    /** 获取当前用户ID */
-    protected JeeUserDetails getCurrentUser(){
+    /**
+     * 获取当前用户ID
+     */
+    protected JeeUserDetails getCurrentUser() {
 
         return (JeeUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    /****获取当前用户Id******/
+    protected String getCurrentMchNo() {
+        return getCurrentUser().getSysUser().getBelongInfoId();
+    }
+
+    /**
+     * 获取当前用户登录IP
+     *
+     * @return
+     */
+    protected String getIp() {
+        return getClientIp();
+    }
+
+
+    protected ApiRes checkIsAdmin() {
+        SysUser sysUser = getCurrentUser().getSysUser();
+
+        if (sysUser.getIsAdmin() != CS.YES) {
+
+            return ApiRes.fail(ApiCodeEnum.SYS_PERMISSION_ERROR);
+        } else {
+            return null;
+        }
     }
 }
