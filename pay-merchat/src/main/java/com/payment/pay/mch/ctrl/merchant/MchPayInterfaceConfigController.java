0706defb1fa09a83b7a4852903599d4826503264
sowrd <15159885771@163.com>
2023-02-01 17:46:23 +0800

    商户支付接口配置类

--- pay-merchat/src/main/java/com/payment/pay/mch/ctrl/merchant/MchPayInterfaceConfigController.java
+++ pay-merchat/src/main/java/com/payment/pay/mch/ctrl/merchant/MchPayInterfaceConfigController.java
@@ -1,13 +1,20 @@
 package com.payment.pay.mch.ctrl.merchant;
 
+import com.alibaba.fastjson.JSONObject;
+import com.pay.components.mq.model.ResetIsvMchAppInfoConfigMQ;
 import com.pay.components.mq.vender.IMQSender;
 import com.pay.pay.core.aop.MethodLog;
+import com.pay.pay.core.constants.ApiCodeEnum;
 import com.pay.pay.core.constants.CS;
+import com.pay.pay.core.entity.MchApp;
 import com.pay.pay.core.entity.MchInfo;
 import com.pay.pay.core.entity.PayInterfaceConfig;
 import com.pay.pay.core.entity.PayInterfaceDefine;
+import com.pay.pay.core.exeception.BizException;
 import com.pay.pay.core.model.ApiRes;
+import com.pay.pay.core.model.DBApplicationConfig;
 import com.pay.pay.core.model.params.NormalMchParams;
+import com.pay.pay.core.utils.StringKit;
 import com.pay.pay.service.impl.MchAppService;
 import com.pay.pay.service.impl.MchInfoService;
 import com.pay.pay.service.impl.PayInterfaceConfigService;
@@ -19,7 +26,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  *商户支付接口配置类
@@ -96,5 +105,82 @@ public class MchPayInterfaceConfigController extends CommonCtrl {
     public ApiRes saveOrUpdate() {
         String ifCode = getValStringRequired("ifCode");
         String infoId = getValStringRequired("infoId");
+
+        PayInterfaceConfig payInterfaceConfig = getObject(PayInterfaceConfig.class);
+        payInterfaceConfig.setInfoType(CS.INFO_TYPE_MCH_APP);
+        payInterfaceConfig.setInfoId(infoId);
+
+        // 存入真实费率
+        if (payInterfaceConfig.getIfRate()!=null) {
+            payInterfaceConfig.setIfRate(payInterfaceConfig.getIfRate().divide(new BigDecimal("100"), 6, BigDecimal.ROUND_HALF_UP));
+        }
+
+        //添加更新者信息
+        Long userId = getCurrentUser().getSysUser().getSysUserId();
+        String realName = getCurrentUser().getSysUser().getRealname();
+        payInterfaceConfig.setUpdatedUid(userId);
+        payInterfaceConfig.setUpdatedBy(realName);
+
+        //根据 商户号、接口类型 获取商户参数配置
+        PayInterfaceConfig dbRecoed = payInterfaceConfigService.getByInfoIdAndIfCode(CS.INFO_TYPE_MCH_APP, infoId, ifCode);
+        //若配置存在，为saveOrUpdate添加ID，第一次配置添加创建者
+        if (dbRecoed != null) {
+            payInterfaceConfig.setId(dbRecoed.getId());
+
+            // 合并支付参数
+            payInterfaceConfig.setIfParams(StringKit.marge(dbRecoed.getIfParams(), payInterfaceConfig.getIfParams()));
+        }else {
+            payInterfaceConfig.setCreatedUid(userId);
+            payInterfaceConfig.setCreatedBy(realName);
+        }
+
+        boolean result = payInterfaceConfigService.saveOrUpdate(payInterfaceConfig);
+        if (!result) {
+            throw new BizException("配置失败");
+        }
+        mqSender.send(ResetIsvMchAppInfoConfigMQ.build(ResetIsvMchAppInfoConfigMQ.RESET_TYPE_MCH_APP, null, getCurrentMchNo(), infoId));
+
+        return ApiRes.ok();
+    }
+
+    /** 查询支付宝商户授权URL **/
+    @GetMapping("/alipayIsvsubMchAuthUrls/{mchAppId}")
+    public ApiRes queryAlipayIsvsubMchAuthUrl(@PathVariable String mchAppId) {
+
+        MchApp mchApp = mchAppService.getById(mchAppId);
+
+        if (mchApp == null || !mchApp.getMchNo().equals(getCurrentMchNo())) {
+            return ApiRes.fail(ApiCodeEnum.SYS_OPERATION_FAIL_SELETE);
+        }
+
+        MchInfo mchInfo = mchInfoService.getById(mchApp.getMchNo());
+        DBApplicationConfig dbApplicationConfig = sysConfigService.getDBApplicationConfig();
+        String authUrl = dbApplicationConfig.genAlipayIsvsubMchAuthUrl(mchInfo.getIsvNo(), mchAppId);
+        String authQrImgUrl = dbApplicationConfig.genScanImgUrl(authUrl);
+
+        JSONObject result = new JSONObject();
+        result.put("authUrl", authUrl);
+        result.put("authQrImgUrl", authQrImgUrl);
+        return ApiRes.ok(result);
+    }
+
+    /** 查询当前应用支持的支付接口 */
+    @PreAuthorize("hasAuthority( 'ENT_DIVISION_RECEIVER_ADD' )")
+    @RequestMapping(value="ifCodes/{appId}", method = RequestMethod.GET)
+    public ApiRes getIfCodeByAppId(@PathVariable("appId") String appId) {
+
+        if(mchAppService.count(MchApp.gw().eq(MchApp::getMchNo, getCurrentMchNo()).eq(MchApp::getAppId, appId)) <= 0){
+            throw new BizException("商户应用不存在");
+        }
+
+        Set<String> result = new HashSet<>();
+
+        payInterfaceConfigService.list(PayInterfaceConfig.gw().select(PayInterfaceConfig::getIfCode)
+                .eq(PayInterfaceConfig::getState, CS.PUB_USABLE)
+                .eq(PayInterfaceConfig::getInfoId, appId)
+                .eq(PayInterfaceConfig::getInfoType, CS.INFO_TYPE_MCH_APP)
+        ).stream().forEach(r -> result.add(r.getIfCode()));
+
+        return ApiRes.ok(result);
     }
 }
