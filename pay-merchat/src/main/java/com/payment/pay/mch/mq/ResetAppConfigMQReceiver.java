666b6b22cb71ddbd664696983ae9fdb0bf5e3785
sowrd <15159885771@163.com>
2023-03-01 18:39:41 +0800

    服务商支付接口管理类

---
+++ pay-merchat/src/main/java/com/payment/pay/mch/mq/ResetAppConfigMQReceiver.java
@@ -0,0 +1,31 @@
+package com.payment.pay.mch.mq;
+
+import com.pay.components.mq.model.ResetAppConfigMQ;
+import com.pay.pay.service.impl.SysConfigService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * Description： 接收MQ消息
+ *
+ * 更新系统配置参数
+ * @author: 段世超
+ * @aate: Created in 2023/3/1 18:29
+ */
+@Component
+@Slf4j
+public class ResetAppConfigMQReceiver implements ResetAppConfigMQ.IMQReceiver {
+
+    @Resource
+    private SysConfigService sysConfigService;
+    @Override
+    public void receive(ResetAppConfigMQ.MsgPayload payload) {
+
+        log.info("成功接收更新系统配置的订阅通知，msg={}" + payload);
+        sysConfigService.initDBConfig(payload.getGroupKey());
+        log.info("系统配置静态属性已重置");
+
+    }
+}
